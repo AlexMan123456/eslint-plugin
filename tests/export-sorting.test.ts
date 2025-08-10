60144c0f14cb08b8c48c84545652e078dd353a57
AlexMan123456 <alex.man03888@gmail.com>
2025-08-10 19:27:06 +0100

    Checkpoint: Add lineBetweenGroups

--- tests/export-sorting.test.ts
+++ tests/export-sorting.test.ts
@@ -12,6 +12,15 @@ standardRuleTester.run("export-sorting", rules["export-sorting"], {
             export type { DropdownMenuProps } from "src/components/DropdownMenu";
             `,
     },
+    // {
+    //     code: stripIndent`
+    //         export { default as CollapsableItem } from "src/components/CollapsableItem";
+    //         export type { CollapsableItemProps } from "src/components/CollapsableItem";
+
+    //         export { default as DropdownMenu } from "src/components/DropdownMenu";
+    //         export type { DropdownMenuProps } from "src/components/DropdownMenu";
+    //     `
+    // }
   ],
   invalid: [
     {
@@ -30,5 +39,33 @@ standardRuleTester.run("export-sorting", rules["export-sorting"], {
         },
       ],
     },
+    // {
+    //     code: stripIndent`
+    //         export { default as CollapsableItem } from "src/components/CollapsableItem";
+    //         export type { CollapsableItemProps } from "src/components/CollapsableItem";
+    //         export { default as DropdownMenu } from "src/components/DropdownMenu";
+    //         export type { DropdownMenuProps } from "src/components/DropdownMenu";
+    //     `,
+    //     output: stripIndent`
+    //         export { default as CollapsableItem } from "src/components/CollapsableItem";
+    //         export type { CollapsableItemProps } from "src/components/CollapsableItem";
+
+    //         export { default as DropdownMenu } from "src/components/DropdownMenu";
+    //         export type { DropdownMenuProps } from "src/components/DropdownMenu";
+    //     `,
+    //     errors: [
+    //         {
+    //             messageId: "lineBetweenGroups",
+    //             data: {
+    //                 source: stripIndent`
+    //                     export { default as CollapsableItem } from "src/components/CollapsableItem";
+    //                     export type { CollapsableItemProps } from "src/components/CollapsableItem";
+    //                     export { default as DropdownMenu } from "src/components/DropdownMenu";
+    //                     export type { DropdownMenuProps } from "src/components/DropdownMenu";
+    //                 `
+    //             }
+    //         }
+    //     ]
+    // }
   ],
 });
