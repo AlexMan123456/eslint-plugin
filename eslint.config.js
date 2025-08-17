b6d619726ae2558298c55411239c6d71a6d6cf42
AlexMan123456 <alex.man03888@gmail.com>
2025-08-17 12:59:32 +0100

    Add comment explaining why we can't access plugin.configs from src/configs

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ export default [
     ...alexPlugin.configs.alexTypeScriptBase,
     {
         rules: {
+            /* When we pass the plugin into the config creator functions from the index, there's no guarantee that the configs will already be
+            on the plugin when you access them from src/configs, since they're in the process of being created. It is generally better to import 
+            another creator function directly from the creator function should you need to refer to another config in the newly added config. */
             "@alextheman/no-plugin-configs-access-from-src-configs": "error"
         }
     }
