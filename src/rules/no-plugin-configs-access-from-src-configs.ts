cbba7c6a84ea74e60ef9dd50a96049793115ae08
AlexMan123456 <alex.man03888@gmail.com>
2025-09-06 02:07:10 +0100

    Get rid of redundant return

--- src/rules/no-plugin-configs-access-from-src-configs.ts
+++ src/rules/no-plugin-configs-access-from-src-configs.ts
@@ -31,7 +31,6 @@ const noPluginConfigAccessFromSrcConfigs = createRule({
                 source: node.object,
               },
             });
-            return;
           }
         }
       },
