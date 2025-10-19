ce89bf02d227592e532a7704c15f4489d53587a2
AlexMan123456 <alex.man03888@gmail.com>
2025-10-19 16:41:46 +0100

    Add React base config and deprecated typeScriptReactBase
    It feels less flexible when we only provide the React config as a TypeScript react config. As such, they've been separated, and users can just bring them together by combining them in an array, as per the deprecation message.

--- src/alexPlugin.ts
+++ src/alexPlugin.ts
@@ -5,9 +5,11 @@ import { name, version } from "package.json";
 import {
   createAlexJavaScriptBaseConfig,
   createAlexPluginBaseConfig,
+  createAlexReactBaseConfig,
   createAlexTypeScriptBaseConfig,
   createAlexTypeScriptReactBaseConfig,
   javaScriptBase,
+  reactBase,
   typeScriptBase,
   typeScriptReactBase,
 } from "src/configs";
@@ -24,9 +26,11 @@ export interface AlexPlugin {
     alexJavaScriptBase: Linter.Config[];
     alexTypeScriptBase: Linter.Config[];
     alexTypeScriptReactBase: Linter.Config[];
+    alexReactBase: Linter.Config[];
     javaScriptBase: Linter.Config[];
     typeScriptBase: Linter.Config[];
     typeScriptReactBase: Linter.Config[];
+    reactBase: Linter.Config[];
   };
   rules: Record<string, any>;
 }
@@ -42,9 +46,11 @@ const alexPlugin: AlexPlugin = {
     alexJavaScriptBase: [] as Linter.Config[],
     alexTypeScriptBase: [] as Linter.Config[],
     alexTypeScriptReactBase: [] as Linter.Config[],
+    alexReactBase: [] as Linter.Config[],
     javaScriptBase: [] as Linter.Config[],
     typeScriptBase: [] as Linter.Config[],
     typeScriptReactBase: [] as Linter.Config[],
+    reactBase: [] as Linter.Config[],
   },
   rules,
 };
@@ -53,10 +59,13 @@ alexPlugin.configs = {
   alexPluginBase: createAlexPluginBaseConfig(alexPlugin),
   alexJavaScriptBase: createAlexJavaScriptBaseConfig(alexPlugin),
   alexTypeScriptBase: createAlexTypeScriptBaseConfig(alexPlugin),
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
   alexTypeScriptReactBase: createAlexTypeScriptReactBaseConfig(alexPlugin),
+  alexReactBase: createAlexReactBaseConfig(alexPlugin),
   javaScriptBase,
   typeScriptBase,
   typeScriptReactBase,
+  reactBase,
 };
 
 export default alexPlugin;
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-deprecated */
 export { default as createAlexJavaScriptBaseConfig } from "src/configs/alexJavaScriptBase";
 export { default as createAlexPluginBaseConfig } from "src/configs/alexPluginBase";
+export { default as createAlexReactBaseConfig } from "src/configs/alexReactBase";
 export { default as createAlexTypeScriptBaseConfig } from "src/configs/alexTypeScriptBase";
 export { default as createAlexTypeScriptReactBaseConfig } from "src/configs/alexTypeScriptReactBase";
 export { default as javaScriptBase } from "src/configs/javaScriptBase";
 export { default as prettierRules } from "src/configs/prettierRules";
+export { default as reactBase } from "src/configs/reactBase";
 export { default as typeScriptBase } from "src/configs/typeScriptBase";
 export { default as typeScriptReactBase } from "src/configs/typeScriptReactBase";
