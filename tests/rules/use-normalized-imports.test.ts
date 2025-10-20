0d87bf7b51dfc920d742048200a3530d52cae7a0
AlexMan123456 <alex.man03888@gmail.com>
2025-10-21 00:16:42 +0100

    Add rule to enforce the use of normalized imports
    This prevents the possibility of ever seeing things like double-slashes or mid-import relative path parts.

---
+++ tests/rules/use-normalized-imports.test.ts
@@ -0,0 +1,96 @@
+import { standardRuleTester } from "tests/rule-testers";
+
+import alexPlugin from "src/index";
+
+const { rules } = alexPlugin;
+
+standardRuleTester.run("use-normalized-imports", rules["use-normalized-imports"], {
+  valid: [
+    {
+      code: 'import Button from "src/components/Button"',
+    },
+    {
+      code: 'import myFunction from "../utility/myFunction"',
+    },
+  ],
+  invalid: [
+    {
+      code: 'import Button from "src/components//Button"',
+      output: 'import Button from "src/components/Button"',
+      errors: [
+        {
+          messageId: "pathNotNormalized",
+          data: {
+            nonNormalized: "src/components//Button",
+            normalized: "src/components/Button",
+          },
+        },
+      ],
+    },
+    {
+      code: 'import myFunction from "src//////utility////myFunction"',
+      output: 'import myFunction from "src/utility/myFunction"',
+      errors: [
+        {
+          messageId: "pathNotNormalized",
+          data: {
+            nonNormalized: "src//////utility////myFunction",
+            normalized: "src/utility/myFunction",
+          },
+        },
+      ],
+    },
+    {
+      code: 'import Button from "src/utility/../components/Button"',
+      output: 'import Button from "src/components/Button"',
+      errors: [
+        {
+          messageId: "pathNotNormalized",
+          data: {
+            nonNormalized: "src/utility/../components/Button",
+            normalized: "src/components/Button",
+          },
+        },
+      ],
+    },
+    {
+      code: "import Button from 'src/utility/../components/Button'",
+      output: "import Button from 'src/components/Button'",
+      errors: [
+        {
+          messageId: "pathNotNormalized",
+          data: {
+            nonNormalized: "src/utility/../components/Button",
+            normalized: "src/components/Button",
+          },
+        },
+      ],
+    },
+    {
+      code: 'import myFunction from "src/./utility/myFunction"',
+      output: 'import myFunction from "src/utility/myFunction"',
+      errors: [
+        {
+          messageId: "pathNotNormalized",
+          data: {
+            nonNormalized: "src/./utility/myFunction",
+            normalized: "src/utility/myFunction",
+          },
+        },
+      ],
+    },
+    {
+      code: 'import Button from "src/./components/../utility/../components/Button"',
+      options: [{ fixable: false }],
+      errors: [
+        {
+          messageId: "pathNotNormalized",
+          data: {
+            nonNormalized: "src/./components/../utility/../components/Button",
+            normalized: "src/components/Button",
+          },
+        },
+      ],
+    },
+  ],
+});
