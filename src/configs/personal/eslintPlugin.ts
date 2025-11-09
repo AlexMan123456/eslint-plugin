1b15536b42b3b4a5e19a4a1ada766c2a8fca01f2
AlexMan123456 <alex.man03888@gmail.com>
2025-11-09 23:46:15 +0000

    Restrict direct imports of the plugin in config files
    This should never really be allowed as it can potentially create circular imports where we try and access the plugin from the configs while it's also trying to define its own configs. The better pattern is to define a function that takes in the current plugin's state, and then returns a config based on that. That way, we never have to reference the plugin directly from the configs themselves.

--- src/configs/personal/eslintPlugin.ts
+++ src/configs/personal/eslintPlugin.ts
@@ -1,17 +1,46 @@
 import type { Linter } from "eslint";
 import type { AlexPlugin } from "src/index";
 
+import perfectionist from "eslint-plugin-perfectionist";
+
 function createPersonalEslintPluginConfig(plugin: AlexPlugin): Linter.Config[] {
+  const noPluginImportMessage =
+    "Do not import the plugin directly from the config files. Please create a function that takes in the plugin and returns the config instead.";
   return [
     {
       plugins: {
         "@alextheman": plugin,
+        perfectionist,
       },
       rules: {
-        // When we pass the plugin into the config creator functions from the index, there's no guarantee that the configs will already be
-        // on the plugin when you access them from src/configs, since they're in the process of being created. It is generally better to import
-        // another creator function directly from the creator function should you need to refer to another config in the newly added config. */
         "@alextheman/no-plugin-configs-access-from-src-configs": "error",
+      },
+    },
+    {
+      files: ["src/configs/**"],
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            paths: [
+              {
+                importNames: ["default"],
+                message: noPluginImportMessage,
+                name: "src/alexPlugin",
+              },
+              {
+                importNames: ["default"],
+                message: noPluginImportMessage,
+                name: "src/index",
+              },
+              {
+                importNames: ["default"],
+                message: noPluginImportMessage,
+                name: "src",
+              },
+            ],
+          },
+        ],
       },
     },
     {
