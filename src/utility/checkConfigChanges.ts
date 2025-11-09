9658f3e8a8722a08cb9c6388148971704380d4d6
AlexMan123456 <alex.man03888@gmail.com>
2025-11-09 23:37:18 +0000

    Separate out the build hashing from the build hash checking
    This allows the build script to create a build hash, so we don't end up repeating the build step in CI either.

--- src/utility/checkConfigChanges.ts
+++ src/utility/checkConfigChanges.ts
@@ -1,27 +1,9 @@
-import crypto from "crypto";
 import fs from "fs";
 import path from "path";
 
-function getFiles(dir: string): string[] {
-  return fs.readdirSync(dir, { withFileTypes: true }).flatMap((entry) => {
-    const fullPath = path.join(dir, entry.name);
-    if (entry.isDirectory()) {
-      return getFiles(fullPath);
-    }
-    return [fullPath];
-  });
-}
+import hashConfigFiles from "src/utility/hashConfigChanges";
 
-function hashFiles(files: string[]): string {
-  const hash = crypto.createHash("sha256");
-  for (const file of files) {
-    hash.update(fs.readFileSync(file));
-  }
-  return hash.digest("hex");
-}
-
-const configFiles = getFiles(path.resolve("src/configs"));
-const currentHash = hashFiles(configFiles);
+const currentHash = hashConfigFiles();
 
 const hashFile = path.resolve(".config-hash");
 const previousHash = fs.existsSync(hashFile) ? fs.readFileSync(hashFile, "utf-8") : null;
