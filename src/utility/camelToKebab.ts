6c71b76ec4404843a43aad9d2b0e81a5f4335d66
AlexMan123456 <alex.man03888@gmail.com>
2025-11-09 02:05:03 +0000

    Type the plugin configs better to allow for autocompletion
    Previously, autocomplete on the config property did not happen due to the string/string typing. This change now refactors the AlexPluginConfigs type such that the keys are extracted away into their own unions, and then we also have a ConfigKey type that knows how to resolve the overall flat type based on the format.

--- src/utility/camelToKebab.ts
+++ src/utility/camelToKebab.ts
@@ -1,4 +1,20 @@
+import type { IgnoreCase } from "@alextheman/utility";
+
 import { camelToKebab as alexCamelToKebab } from "@alextheman/utility";
+
+export type ToKebabCase<S extends string> = S extends `${IgnoreCase<"J">}avaScript${infer Rest}`
+  ? Rest extends ""
+    ? "javascript"
+    : `javascript${ToKebabCase<Rest>}`
+  : S extends `${IgnoreCase<"T">}ypeScript${infer Rest}`
+    ? Rest extends ""
+      ? "typescript"
+      : `typescript${ToKebabCase<Rest>}`
+    : S extends `${infer Head}${infer Tail}`
+      ? Head extends Lowercase<Head>
+        ? `${Head}${ToKebabCase<Tail>}`
+        : `-${Lowercase<Head>}${ToKebabCase<Tail>}`
+      : S;
 
 function camelToKebab(string: string): string {
   return alexCamelToKebab(
