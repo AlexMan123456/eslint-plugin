ab406cc57ad74866f56a55e6fdb0c9d694040fb4
AlexMan123456 <alex.man03888@gmail.com>
2025-11-13 23:35:45 +0000

    Add a custom default bannerComment to the auto-generated config rule options types.

--- src/utility/generateRuleOptionsTypes.ts
+++ src/utility/generateRuleOptionsTypes.ts
@@ -14,7 +14,10 @@ const rulesTypesDir = "src/rules/types";
   for (const [ruleName, rule] of Object.entries(alexPlugin.rules)) {
     const typeName = `${kebabToCamel(ruleName, { startWithUpper: true })}Options`;
     if (rule.meta.schema[0]) {
-      const typeFileContents = await compile(rule.meta.schema[0], typeName);
+      const typeFileContents = await compile(rule.meta.schema[0], typeName, {
+        bannerComment:
+          "/** Generated from src/utility/generateRuleOptionsTypes.ts.\n* You may execute the above file to re-generate this type using `npm run generate-rule-options-types` */",
+      });
       await writeFile(path.join(process.cwd(), rulesTypesDir, `${typeName}.ts`), typeFileContents);
       await appendFile(
         path.join(process.cwd(), rulesTypesDir, "index.ts"),
