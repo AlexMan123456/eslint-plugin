1b90c6cfffd3da047b0d7888326e42938308378e
AlexMan123456 <alex.man03888@gmail.com>
2025-11-13 23:26:21 +0000

    Disable import/no-unresolved in general TypeScript config
    Not really needed here as the TypeScript compiler catches unused imports for us already.

--- src/configs/general/typeScriptBase.ts
+++ src/configs/general/typeScriptBase.ts
@@ -20,18 +20,14 @@ const typeScriptBase: Linter.Config[] = [
       "@typescript-eslint/no-deprecated": "warn",
       "@typescript-eslint/no-redeclare": ["error", { ignoreDeclarationMerge: true }],
       "@typescript-eslint/no-unused-vars": ["error", unusedVarsIgnorePatterns],
+      // Disable import/no-unresolved (enabled in general/javascript, being extended here) because the TypeScript compiler already catches unused imports for us.
+      "import/no-unresolved": "off",
       // Disable regular no-redeclare in favour of TypeScript-specific version since that will flag type redeclarations when we do const MyType = {}; export type MyType = ...
       "no-redeclare": "off",
       // Disable no-undef because undefined variables tend to be better caught by the TypeScript compiler.
       "no-undef": "off",
       // Disable regular no-unused-vars rule since that will flag interface declarations. Only use the TypeScript specific rule for this.
       "no-unused-vars": "off",
-    },
-    settings: {
-      "import/resolver": {
-        node: true,
-        typescript: true,
-      },
     },
   },
 ];
