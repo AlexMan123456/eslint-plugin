7688ec75add54d9b6e242619dee8266e4db29142
AlexMan123456 <alex.man03888@gmail.com>
2025-11-27 22:18:06 +0900

    Switch the no-namespace-imports tests to use eslint-vitest-rule-tester
    I think this is better than the previous usage of @typescript-eslint/rule-tester directly as this allows us to structure the test suite in the classic describe, test, expect way rather than with a giant ruleTester.run invocation that is harder to control. This approach opens up the possibility of us being able to use something like tempy for ruleTesterWithParser tests where we may need file directory information.

---
+++ tests/rule-testers/createRuleTester.ts
@@ -0,0 +1,18 @@
+import type { RuleTester, RuleTesterInitOptions } from "eslint-vitest-rule-tester";
+
+import { createRuleTester as createVitestRuleTester } from "eslint-vitest-rule-tester";
+
+function createRuleTester<RuleOptions = any, MessageId extends string = string>(
+  options: RuleTesterInitOptions,
+): RuleTester<RuleOptions, MessageId> {
+  const { languageOptions } = options;
+  return createVitestRuleTester({
+    languageOptions: {
+      ecmaVersion: "latest",
+      ...languageOptions,
+    },
+    ...options,
+  });
+}
+
+export default createRuleTester;
