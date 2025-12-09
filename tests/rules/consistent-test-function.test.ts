05c43b4dd2d807fb918c66d42785faa960fa1df9
AlexMan123456 <alex.man03888@gmail.com>
2025-12-09 00:02:03 +0000

    Refactor to use eslint-vitest-rule-tester for an explicit test suite
    Previously I was using @typescript-eslint/rule-tester, which did not allow for as much control over the test suite. with eslint-vitest-rule-tester, we can now instead write out the tests using the usual pattern, allowing us to set our own test messages and potentially do some extra logic before we test the rule should we need to (e.g. set up a temp directory).

--- tests/rules/consistent-test-function.test.ts
+++ tests/rules/consistent-test-function.test.ts
@@ -1,158 +1,175 @@
-import { stripIndent } from "common-tags";
-import { standardRuleTester } from "tests/rule-testers";
+import type { ConsistentTestFunctionOptions } from "src/index";
+
+import { normaliseIndents } from "@alextheman/utility";
+import createRuleTester from "tests/rule-testers/createRuleTester";
+import { describe, test } from "vitest";
 
 import rules from "src/rules";
 
-standardRuleTester.run("consistent-test-function", rules["consistent-test-function"], {
-  valid: [
-    {
-      code: stripIndent`
-            import { test } from "vitest";
+describe("consistent-test-function", () => {
+  const { valid, invalid } = createRuleTester<ConsistentTestFunctionOptions[]>({
+    name: "consistent-test-function",
+    rule: rules["consistent-test-function"],
+  });
+
+  describe("Valid", () => {
+    test("Allows test when preference is set to test", () => {
+      valid({
+        code: normaliseIndents`
             test("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      options: [
-        {
-          preference: "test",
-        },
-      ],
-    },
-    {
-      code: stripIndent`
-            import { test } from "vitest";
-            test("Does a thing", () => {
-                expect(1).toBe(1);
-            });
-            `,
-    },
-    {
-      code: stripIndent`
-            test("Does a thing", () => {
-                expect(1).toBe(1);
-            });
-            `,
-      options: [
-        {
-          preference: "test",
-        },
-      ],
-    },
-    {
-      code: stripIndent`
-            it("Does a thing", () => {
-                expect(1).toBe(1);
-            });
-            `,
-      options: [
-        {
-          preference: "it",
-        },
-      ],
-    },
-    {
-      code: stripIndent`
-            import { it } from "vitest";
-            it("Does a thing", () => {
-                expect(1).toBe(1);
-            });
-            `,
-      options: [
-        {
-          preference: "it",
-        },
-      ],
-    },
-  ],
-  invalid: [
-    {
-      code: stripIndent`
-            it("Does a thing", () => {
-                expect(1).toBe(1);
-            });
-            `,
-      output: stripIndent`
-            test("Does a thing", () => {
-                expect(1).toBe(1);
-            });
-            `,
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "it",
+        options: [
+          {
             preference: "test",
           },
-        },
-      ],
-    },
-    {
-      code: stripIndent`
-            import { it } from "vitest";
-            it("Does a thing", () => {
-                expect(1).toBe(1);
-            });
-            `,
-      output: stripIndent`
+        ],
+      });
+    });
+    test("Defaults to test if preference is not set", () => {
+      valid(normaliseIndents`
             import { test } from "vitest";
             test("Does a thing", () => {
                 expect(1).toBe(1);
             });
-            `,
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "it",
-            preference: "test",
-          },
-        },
-        {
-          messageId: "message",
-          data: {
-            source: "it",
-            preference: "test",
-          },
-        },
-      ],
-    },
-    {
-      code: stripIndent`
+            `);
+    });
+    test("Allows imported usage of test", () => {
+      valid(normaliseIndents`
             import { test } from "vitest";
             test("Does a thing", () => {
                 expect(1).toBe(1);
             });
-            `,
-      options: [
-        {
-          preference: "it",
-        },
-      ],
-      output: stripIndent`
-            import { it } from "vitest";
+            `);
+    });
+    test("Allows it when preference is set to it", () => {
+      valid({
+        code: normaliseIndents`
             it("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "test",
+        options: [
+          {
             preference: "it",
           },
-        },
-        {
-          messageId: "message",
-          data: {
-            source: "test",
+        ],
+      });
+    });
+    test("Allows imported it usage when preference is set to it", () => {
+      valid({
+        code: normaliseIndents`
+            import { it } from "vitest";
+            it("Does a thing", () => {
+                expect(1).toBe(1);
+            });
+            `,
+        options: [
+          {
             preference: "it",
           },
-        },
-      ],
-    },
-    {
-      code: stripIndent`
+        ],
+      });
+    });
+  });
+
+  describe("Invalid", () => {
+    test("Does not allow it and changes it to test if no preference is set (defaults to test)", () => {
+      invalid({
+        code: normaliseIndents`
+            it("Does a thing", () => {
+                expect(1).toBe(1);
+            });
+            `,
+        output: normaliseIndents`
+            test("Does a thing", () => {
+                expect(1).toBe(1);
+            });
+            `,
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
+          },
+        ],
+      });
+    });
+    test("Changes imports from it to test", () => {
+      invalid({
+        code: normaliseIndents`
+            import { it } from "vitest";
+            it("Does a thing", () => {
+                expect(1).toBe(1);
+            });
+            `,
+        output: normaliseIndents`
+            import { test } from "vitest";
+            test("Does a thing", () => {
+                expect(1).toBe(1);
+            });
+            `,
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
+          },
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
+          },
+        ],
+      });
+    });
+    test("Changes test to it if preference set to it", () => {
+      invalid({
+        code: normaliseIndents`
+            import { test } from "vitest";
+            test("Does a thing", () => {
+                expect(1).toBe(1);
+            });
+            `,
+        options: [
+          {
+            preference: "it",
+          },
+        ],
+        output: normaliseIndents`
+            import { it } from "vitest";
+            it("Does a thing", () => {
+                expect(1).toBe(1);
+            });
+            `,
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "test",
+              preference: "it",
+            },
+          },
+          {
+            messageId: "message",
+            data: {
+              source: "test",
+              preference: "it",
+            },
+          },
+        ],
+      });
+    });
+    test("If test and it imported, only get rid of it and change it usage to test", () => {
+      invalid({
+        code: normaliseIndents`
             import { test, it } from "vitest"
             it("Does a thing", () => {
                 expect(1).toBe(1);
@@ -161,7 +178,7 @@ standardRuleTester.run("consistent-test-function", rules["consistent-test-functi
                 expect(2).toBe(2);
             })
             `,
-      output: stripIndent`
+        output: normaliseIndents`
             import { test } from "vitest"
             test("Does a thing", () => {
                 expect(1).toBe(1);
@@ -170,132 +187,141 @@ standardRuleTester.run("consistent-test-function", rules["consistent-test-functi
                 expect(2).toBe(2);
             })
             `,
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "it",
-            preference: "test",
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
           },
-        },
-        {
-          messageId: "message",
-          data: {
-            source: "it",
-            preference: "test",
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
           },
-        },
-      ],
-    },
-    {
-      code: stripIndent`
+        ],
+      });
+    });
+    test("Changes the base aliasing", () => {
+      invalid({
+        code: normaliseIndents`
             import { it as testFunction } from "vitest";
             testFunction("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      output: stripIndent`
+        output: normaliseIndents`
             import { test as testFunction } from "vitest";
             testFunction("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "it",
-            preference: "test",
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
           },
-        },
-      ],
-    },
-    {
-      code: stripIndent`
+        ],
+      });
+    });
+    test("Changes the base aliasing when test is used when preference set to it", () => {
+      invalid({
+        code: normaliseIndents`
             import { test as itFunction } from "vitest";
             itFunction("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      options: [
-        {
-          preference: "it",
-        },
-      ],
-      output: stripIndent`
+        options: [
+          {
+            preference: "it",
+          },
+        ],
+        output: normaliseIndents`
             import { it as itFunction } from "vitest";
             itFunction("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "test",
-            preference: "it",
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "test",
+              preference: "it",
+            },
           },
-        },
-      ],
-    },
-    {
-      code: stripIndent`
+        ],
+      });
+    });
+    test("Does not autofix if fixable is set to false", () => {
+      invalid({
+        code: normaliseIndents`
             import { it } from "vitest";
             it("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      options: [
-        {
-          fixable: false,
-        },
-      ],
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "it",
-            preference: "test",
+        options: [
+          {
+            fixable: false,
           },
-        },
-        {
-          messageId: "message",
-          data: {
-            source: "it",
-            preference: "test",
+        ],
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
           },
-        },
-      ],
-    },
-    {
-      code: stripIndent`
+          {
+            messageId: "message",
+            data: {
+              source: "it",
+              preference: "test",
+            },
+          },
+        ],
+      });
+    });
+    test("Does not autofix is preference set to it and fixable is set to false", () => {
+      invalid({
+        code: normaliseIndents`
             import { test } from "vitest";
             test("Does a thing", () => {
                 expect(1).toBe(1);
             });
             `,
-      options: [
-        {
-          preference: "it",
-          fixable: false,
-        },
-      ],
-      errors: [
-        {
-          messageId: "message",
-          data: {
-            source: "test",
+        options: [
+          {
             preference: "it",
+            fixable: false,
           },
-        },
-        {
-          messageId: "message",
-          data: {
-            source: "test",
-            preference: "it",
+        ],
+        errors: [
+          {
+            messageId: "message",
+            data: {
+              source: "test",
+              preference: "it",
+            },
           },
-        },
-      ],
-    },
-  ],
+          {
+            messageId: "message",
+            data: {
+              source: "test",
+              preference: "it",
+            },
+          },
+        ],
+      });
+    });
+  });
 });
