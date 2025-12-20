042fac2e07f41f94ed56e1a029b728f58af6dd8d
AlexMan123456 <alex.man03888@gmail.com>
2025-12-20 23:22:05 +0000

    Rename all types specific to my plugin to be prefixed with 'Alex'
    This will make it a lot clearer that these types affect my plugin specifically. This will be helpful especially as I start to make the main createConfigGroup and createPluginConfigs function more generic so that they work for any plugin.

--- src/utility/private/createConfigGroup.ts
+++ src/utility/private/createConfigGroup.ts
@@ -1,13 +1,13 @@
 import type { Linter } from "eslint";
 
-import type { ConfigGroupName, ConfigKey } from "src/configs/AlexPluginConfigGroup";
+import type { AlexConfigGroupName, AlexConfigKey } from "src/configs/AlexPluginConfigGroup";
 
 import camelToKebab from "src/utility/private/camelToKebab";
 
 function createConfigGroup(
-  group: ConfigGroupName,
+  group: AlexConfigGroupName,
   configs: Record<string, Linter.Config[]>,
-): Record<ConfigKey, Linter.Config[]> {
+): Record<AlexConfigKey, Linter.Config[]> {
   const newConfigs: Record<string, Linter.Config[]> = {};
   for (const key in configs) {
     newConfigs[`${camelToKebab(group)}/${camelToKebab(key)}`] = configs[key];
