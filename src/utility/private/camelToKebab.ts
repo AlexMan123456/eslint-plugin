88b6eb07ac88734789277aa6266ffc135af7ac3d
AlexMan123456 <alex.man03888@gmail.com>
2025-12-21 02:24:59 +0000

    Make the camelToKebab usage more consistent
    Previously, there was a mismatch between compile-time and runtime, where consecutive capitals were being preserved at runtime, but not compile-time. This was fine for personal usage, but now that it is being used in public helpers, it would be best if the compile-time and runtime were more in-sync.
    
    Also, note that I have fixed the conversion of TypeScript and JavaScript so they get replaced starting with the capital letter. This is important otherwise the conversion will be inaccurate (e.g. myTypeScriptProject would become mytypescriptProject and therefore mytypescript-project previously)

--- src/utility/private/camelToKebab.ts
+++ src/utility/private/camelToKebab.ts
@@ -1,15 +1,15 @@
 import type { IgnoreCase } from "@alextheman/utility";
 
-import { camelToKebab as alexCamelToKebab } from "@alextheman/utility";
+import { camelToKebab as alexCamelToKebab, DataError } from "@alextheman/utility";
 
 export type CamelToKebab<S extends string> = S extends `${IgnoreCase<"J">}avaScript${infer Rest}`
   ? Rest extends ""
-    ? "javascript"
-    : `javascript${CamelToKebab<Rest>}`
+    ? "-javascript"
+    : `-javascript${CamelToKebab<Rest>}`
   : S extends `${IgnoreCase<"T">}ypeScript${infer Rest}`
     ? Rest extends ""
-      ? "typescript"
-      : `typescript${CamelToKebab<Rest>}`
+      ? "-typescript"
+      : `-typescript${CamelToKebab<Rest>}`
     : S extends `${infer Head}${infer Tail}`
       ? Head extends Lowercase<Head>
         ? `${Head}${CamelToKebab<Tail>}`
@@ -17,8 +17,16 @@ export type CamelToKebab<S extends string> = S extends `${IgnoreCase<"J">}avaScr
       : S;
 
 function camelToKebab(string: string): string {
+  if (string[0] === string[0].toUpperCase()) {
+    throw new DataError(
+      string,
+      "CAMEL_TO_KEBAB_CONVERSION_ERROR",
+      "camelCase string must start with a lowercase letter.",
+    );
+  }
   return alexCamelToKebab(
-    string.replace(/[Tt]ypeScript/, "typescript").replace(/[Jj]avaScript/, "javascript"),
+    string.replace(/[Tt]ypeScript/, "Typescript").replace(/[Jj]avaScript/, "Javascript"),
+    { preserveConsecutiveCapitals: false },
   );
 }
 
