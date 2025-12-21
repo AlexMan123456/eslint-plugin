3c16c24be0a5b588ef91b05aa2cfa714bb89564f
AlexMan123456 <alex.man03888@gmail.com>
2025-12-21 03:31:53 +0000

    Add exports of new utility stuff

--- src/utility/public/index.ts
+++ src/utility/public/index.ts
@@ -1,4 +1,7 @@
 export { default as checkCallExpression } from "src/utility/public/checkCallExpression";
 export { default as combineRestrictedImports } from "src/utility/public/combineRestrictedImports";
 export { default as createRuleSchemaFromZodSchema } from "src/utility/public/createRuleSchemaFromZodSchema";
+export { default as flattenConfigs } from "src/utility/public/flattenConfigs";
 export { default as getImportSpecifiersAfterRemoving } from "src/utility/public/getImportSpecifiersAfterRemoving";
+
+export type { ConfigKey } from "src/utility/public/ConfigKey";
