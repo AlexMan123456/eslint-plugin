7038c356b2195b35405457417f577e04ea665a55
AlexMan123456 <alex.man03888@gmail.com>
2025-12-21 03:41:22 +0000

    Rename AlexPluginConfigGroup to AlexPluginConfigObject and introduce AlexPluginConfigFlattened

--- src/configs/AlexPluginConfigGroup.ts
+++ src/configs/AlexPluginConfigGroup.ts
@@ -25,13 +25,14 @@ export type CombinedConfig =
   | "javaScriptReact"
   | "typeScriptPackage";
 
-export interface AlexPluginConfigGroup {
+export interface AlexPluginConfigObject {
   general: Record<GeneralConfig, Linter.Config[]>;
   plugin: Record<PluginConfig, Linter.Config[]>;
   personal: Record<PersonalConfig, Linter.Config[]>;
   combined: Record<CombinedConfig, Linter.Config[]>;
 }
 
-export type AlexConfigGroupName = keyof AlexPluginConfigGroup;
+export type AlexConfigGroupName = keyof AlexPluginConfigObject;
 
-export type AlexConfigKey = ConfigKey<AlexPluginConfigGroup>;
+export type AlexConfigKey = ConfigKey<AlexPluginConfigObject>;
+export type AlexPluginConfigFlattened = Record<AlexConfigKey, Linter.Config[]>;
